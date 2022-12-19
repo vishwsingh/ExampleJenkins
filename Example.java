69bd8f5e64be6110b164fdab8879ff98898def87
vishwsingh <118203821+vishwsingh@users.noreply.github.com>
2022-12-19 15:59:41 -0500

    Update Example.java

--- Example.java
+++ Example.java
@@ -3,5 +3,7 @@ public class Example {
   public static void main(String[] args) {
   
   System.out.println("Hi, this is new example");
+    
+    System.out.println("Adding another example");
   }
 }
